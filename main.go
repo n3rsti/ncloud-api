2bcf14302d1713b74053ec8cab895881dcacdff4
n3rsti <n3rsti@protonmail.com>
2023-01-06 14:41:22 +0100

    import gin create basic router with healthcheck endpoint

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func health(c *gin.Context){
+	c.IndentedJSON(http.StatusOK, map[string]string{"ok": "true"})
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/api/health", health)
+
+
+	router.Run("localhost:8080")
+}
