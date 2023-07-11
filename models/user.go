be9c9a5acfb15994a0942ba539b25e7946d375f4
n3rsti <n3rsti@protonmail.com>
2023-07-11 23:53:17 +0200

    format models/user

--- models/user.go
+++ models/user.go
@@ -5,9 +5,9 @@ import (
 )
 
 type User struct {
-	Username         string `json:"username" validate:"min=1"`
-	Password         string `json:"password,omitempty" validate:"min=5"`
-	TrashAccessKey   string `json:"trash_access_key,omitempty"`
+	Username       string `json:"username"                   validate:"min=1"`
+	Password       string `json:"password,omitempty"         validate:"min=5"`
+	TrashAccessKey string `json:"trash_access_key,omitempty"`
 }
 
 func (u *User) ToBSON() bson.D {
