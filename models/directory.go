40cd09fc584dc9b9ae4798539248c63bf1004b4f
n3rsti <n3rsti@protonmail.com>
2023-07-31 15:51:20 +0200

    remove unused import

--- models/directory.go
+++ models/directory.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
